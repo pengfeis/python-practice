6c82807673ea6b43548fb8d31c55351efce7a729
pengfeis <supengfei007@gmail.com>
2014-09-03 01:53:41 +0800

    email use python3

--- learning-python.py
+++ learning-python.py
@@ -1,6 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+#coding: utf-8
+import smtplib
+from email.mime.text import MIMEText
 
-a, b = 0, 1
-while b < 1000:
-    print(b, end=',')
-    a, b = b, a+b
+sender = 'supengfei007@163.com'
+receiver = 'supengfei007@gmail.com'
+subject = 'python email test'
+smtpserver = 'smtp.163.com'
+username = 'supengfei007@163.com'
+password = '********'
+
+msg = MIMEText('<html><h1>Hello World</h1></html>','html','utf-8')
+
+msg['Subject'] = subject
+
+smtp = smtplib.SMTP()
+smtp.connect('smtp.163.com')
+smtp.login(username, password)
+smtp.sendmail(sender, receiver, msg.as_string())
+smtp.quit()
