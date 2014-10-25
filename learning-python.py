2d34265a5b07bfa0ae3efbad88fab173d2fee084
pengfeis <supengfei007@gmail.com>
2014-10-25 22:09:22 +0800

    django tutorial sample
    This is a django tutorial sample app

--- learning-python.py
+++ learning-python.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python3
 #coding: utf-8
-import smtplib
-from email.mime.text import MIMEText
 
-sender = 'supengfei007@163.com'
-receiver = 'supengfei007@gmail.com'
-subject = 'python email test'
-smtpserver = 'smtp.163.com'
-username = 'supengfei007@163.com'
-password = '********'
+l = []
+n = 1
 
-msg = MIMEText('<html><h1>Hello World</h1></html>','html','utf-8')
+while n <= 99:
+    l.append(n)
+    n = n + 2
 
-msg['Subject'] = subject
-
-smtp = smtplib.SMTP()
-smtp.connect('smtp.163.com')
-smtp.login(username, password)
-smtp.sendmail(sender, receiver, msg.as_string())
-smtp.quit()
+print(l)
